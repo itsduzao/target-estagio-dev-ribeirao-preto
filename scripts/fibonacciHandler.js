9b07053572c521b7d64af739cea6d57ff541076e
Eduardo Henrique <itsduzao@outlook.com>
2024-09-23 12:23:54 -0300

    feat: add fibonacci handler

---
+++ scripts/fibonacciHandler.js
@@ -0,0 +1,18 @@
+import { fibonacciSequenceGenerator } from './fibonacciSequenceGenerator.js';
+import { isInFibonacci } from './isIsFibonacci.js';
+import { displayResultMessage } from './displayResultMessage.js';
+import { showElement } from './showElement.js';
+
+const fibonacciButton = document.getElementById('fibonacciButton')
+fibonacciButton.addEventListener("click", fibonacciHandler)
+
+export function fibonacciHandler(){
+  const fibonacciInput = Number(document.querySelector("#fibonacciInput").value)
+  const fibonacciSequence = fibonacciSequenceGenerator(fibonacciInput)
+  const result = isInFibonacci(fibonacciSequence, fibonacciInput)
+  const message = result ? `O número faz parte da sequência` : `O número não faz parte da sequência`
+  const fibonacciOutput = document.querySelector('#fibonacciOutput')
+  showElement(fibonacciOutput)
+  displayResultMessage(message, fibonacciOutput)
+}
+
---
+++ scripts/index.js
@@ -0,0 +1,1 @@
+import { fibonacciHandler } from './fibonacciHandler.js';
