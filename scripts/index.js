fe508db29ec8dfdf09efbaaeca5e138115e7f0af
Eduardo Henrique <itsduzao@outlook.com>
2024-09-27 16:19:00 -0300

    Atualizar o index.js
    Chore: add todo task as comment

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+TODO: add subfolders according to script use case
+
 import { fibonacciHandler } from './fibonacciHandler.js';
 import { containsLetterHandler } from './containsLetterHandler.js';
 import { sumValuesUpToLimitHandler } from './sumValuesUpToLimitHandler.js';
