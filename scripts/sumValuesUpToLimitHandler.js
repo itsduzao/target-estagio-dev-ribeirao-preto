8bcd7ee6ca6a9f0081a69bdfe0c93c54fc0ccae3
Eduardo Henrique <itsduzao@outlook.com>
2024-09-30 18:42:41 -0300

    feat: add new empty field validator and flexible result message

--- scripts/sumValuesUpToLimitHandler.js
+++ scripts/sumValuesUpToLimitHandler.js
@@ -2,28 +2,32 @@ import { sumValuesUpToLimit } from './sumValuesUpToLimit.js';
 import { showElement } from './showElement.js';
 import { displayResultMessage } from './displayResultMessage.js';
 import { isEmpty } from './isEmpty.js';
+import { returnEmptyFields } from './returnEmptyFields.js';
+import { createFlexibeMessage } from './CreateFlexibeMessage.js';
 
 export function sumValuesUpToLimitHandler(){
-  const indexInput = document.getElementById('indexInput').value
-  const countInput = document.getElementById('countInput').value
-  const accmulatorInput = document.getElementById('accumulatorInput').value
+  const indexInput = document.getElementById('indexInput')
+  const countInput = document.getElementById('countInput')
+  const accumulatorInput = document.getElementById('accumulatorInput')
 
+  const isFieldEmpty = isEmpty(indexInput.value) || isEmpty(countInput.value) || isEmpty(accumulatorInput.value)
+  
   const output = document.querySelector('#sumValuesUpToLimitOutput')
 
-  const isFieldEmpty = isEmpty({indexInput, countInput, accmulatorInput})
-  console.log(isFieldEmpty)
-
   if (!isFieldEmpty) {
-    const limit = Number(indexInput)
-    const count = Number(countInput)
-    const accumulator = Number(accmulatorInput)
-    const result = sumValuesUpToLimit({limit, count, accumulator})
+    const limit = Number(indexInput.value)
+    const count = Number(countInput.value)
+    const accumulator = Number(accumulatorInput.value)
+    const result = sumValuesUpToLimit({ limit, count, accumulator })
     const message = `O valor da variável SOMA é ${result}.`
     showElement(output)
     return displayResultMessage(message, output)
   }
 
-  const message = `Número não informado`
+  const inputFields = [indexInput, accumulatorInput, countInput]
+  const emptyFields = returnEmptyFields(inputFields)
+  const message = createFlexibeMessage(emptyFields)
+
   showElement(output)
   displayResultMessage(message, output)
 }
