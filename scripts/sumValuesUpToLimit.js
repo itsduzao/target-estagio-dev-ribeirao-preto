382584b6075afd9979a2d73cf63ff26fd8e67550
Eduardo Henrique <itsduzao@outlook.com>
2024-09-30 14:15:36 -0300

    style: remove unused console log statement

--- scripts/sumValuesUpToLimit.js
+++ scripts/sumValuesUpToLimit.js
@@ -2,7 +2,6 @@ export function sumValuesUpToLimit({limit,count,accumulator}){
   while (count < limit) {
     count++
     accumulator += count
-    console.log({count,accumulator})
   }
 
   return accumulator
