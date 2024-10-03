8997c52d1fed1228b44a83956a1bdf6fb8459879
Eduardo Henrique <itsduzao@outlook.com>
2024-10-03 10:04:57 -0300

    add verification if element contains hidden class

--- scripts/router/index.js
+++ scripts/router/index.js
@@ -3,21 +3,25 @@ export function router() {
   const questions = document.querySelectorAll('.question-container')
   const initialContent = document.querySelector('.initial-content')
   const companyBrand = document.querySelector('.company-brand-container')
+  const questionsOutput = document.querySelectorAll('.output-wrapper')
 
   function showContent(contentToShow) {
-    questions.forEach(q => q.classList.add('hidden'));
-    initialContent.classList.add('hidden');
-    contentToShow.classList.remove('hidden');
+    if (contentToShow.classList.contains('hidden')) {
+      questions.forEach(q => q.classList.add('hidden'))
+      initialContent.classList.add('hidden')
+      questionsOutput.forEach(qOutput => qOutput.classList.add('hidden'))
+      contentToShow.classList.remove('hidden')
+    }
   }
 
   links.forEach(link => {
     link.addEventListener('click', (e) => {
       e.preventDefault()
       const targetQuestion = e.target.textContent
-      
+
       links.forEach(l => l.classList.remove('active'))
       e.target.classList.add('active')
-      
+
       const questionToShow = document.querySelector(`.question-container:nth-child(${targetQuestion.slice(1)})`)
       if (questionToShow) {
         showContent(questionToShow)
