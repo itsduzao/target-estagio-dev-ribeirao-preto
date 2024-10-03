48e1772cdfec3e3f353c76eb8794b944160ba76a
Eduardo Henrique <itsduzao@outlook.com>
2024-10-02 22:26:55 -0300

    feat: add hidden class toggler

---
+++ scripts/router/index.js
@@ -0,0 +1,32 @@
+export function router() {
+  const links = document.querySelectorAll('.question-link')
+  const questions = document.querySelectorAll('.question-container')
+  const initialContent = document.querySelector('.initial-content')
+  const companyBrand = document.querySelector('.company-brand-container')
+
+  function showContent(contentToShow) {
+    questions.forEach(q => q.classList.add('hidden'));
+    initialContent.classList.add('hidden');
+    contentToShow.classList.remove('hidden');
+  }
+
+  links.forEach(link => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault()
+      const targetQuestion = e.target.textContent
+      
+      links.forEach(l => l.classList.remove('active'))
+      e.target.classList.add('active')
+      
+      const questionToShow = document.querySelector(`.question-container:nth-child(${targetQuestion.slice(1)})`)
+      if (questionToShow) {
+        showContent(questionToShow)
+      }
+    })
+  })
+
+  companyBrand.addEventListener('click', () => {
+    links.forEach(link => link.classList.remove('active'))
+    showContent(initialContent)
+  })
+}
